6473d5ed19d78dd99e7cb3945f37ddecab4dbe79
prototypedave <popupgfj@gmail.com>
2022-04-10 16:59:07 +0000

    Flask web Application

---
+++ web_flask/1-hbnb_route.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+starts flask web application
+"""
+
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """returns HBNB"""
+    return 'HBNB'
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port='5000')
+
